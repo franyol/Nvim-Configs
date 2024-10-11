a5d809d9cfba2822b7dbd0a63dcc196a7086f2e0
Francisco Javier Valbuena Ortega <francisco.valbuena@kiwibot.com>
2024-10-11 00:17:27 -0500

    Update lsp.lua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,13 +2,27 @@ local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
+
+  -- Use floating window for diagnostic messages
+  vim.diagnostic.config({
+    virtual_text = false, -- Disable virtual text (the one on the same line as the error)
+    signs = true,         -- Keep the signs in the sign column
+    update_in_insert = false,
+    float = {
+      source = "always",  -- Show the source of the diagnostic in the floating window
+      border = "rounded", -- Add a border to the floating window
+    },
+  })
+
+  -- Map keys to show diagnostics in a floating window
+  vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {buffer = bufnr, noremap = true, silent = true})
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'tsserver', 'rust_analyzer', 'eslint', 'bashls', 
+  ensure_installed = {'tsserver', 'rust_analyzer', 'biome', 'bashls', 
   		      'clangd', 'cmake', 'dockerls', 'html', 'jsonls', 
 		      'lua_ls', 'autotools_ls',
 	              'marksman', 'pyright', 'sqls', 'lemminx', 'yamlls'},
