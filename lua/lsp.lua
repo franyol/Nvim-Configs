428197eea4cf8e159ecf77fcfe2cc2634b95aeb6
Francisco Javier Valbuena Ortega <francisco.valbuena@kiwibot.com>
2025-02-01 14:22:22 -0500

    Update lsp.lua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -22,13 +22,19 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'rust_analyzer', 'biome', 'bashls', 
-  		      'clangd', 'cmake', 'dockerls', 'html', 'jsonls', 
+  ensure_installed = {'rust_analyzer', 'biome', 'bashls',
+  		      'clangd', 'cmake', 'dockerls', 'html', 'jsonls',
 		      'lua_ls', 'autotools_ls',
-	              'marksman', 'pyright', 'sqls', 'lemminx', 'yamlls'},
+	              'marksman', 'pyright', 'lemminx', 'yamlls'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
     end,
   }
 })
+require('lspconfig').biome.setup{
+  root_dir = function(fname)
+    local root = require('lspconfig.util').root_pattern('package.json')(fname)
+    return root or vim.fn.getcwd()  -- Default to current directory if no package.json is found
+  end
+}
